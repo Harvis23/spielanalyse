eb4be1243d31e82b628f1ad2efdae13314759d5b
Harvis Castellanos <harvis.castellanos@gmail.com>
2024-07-08 15:48:45 +0200

    Fixed typo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from trackers import Tracker
 
 def main():
     # Read Video
-    video_frames = read_video('input_videos/08fd33_4.mp4')
+    video_frames = read_video('input_videos/08fd33_8.mp4')
 
     # Initialize Tracker
     tracker = Tracker('models/best.pt')
@@ -11,9 +11,13 @@ def main():
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
-
+    
+    #Draw output
+    ##Draw object tracks
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+    
     # Save video
-    save_video(video_frames, 'output_videos/output_video.avi')
+    save_video(output_video_frames, 'output_videos/output_video.avi')
 
 
 
