ef985d7b09ae332f87bf3f5d0b13536d0c1d2a4f
Harvis Castellanos <harvis.castellanos@gmail.com>
2024-07-10 13:55:46 +0200

    Assign player team color

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import read_video, save_video 
 from trackers import Tracker
+from team_asssigner import TeamAssigner
 import cv2
 
 def main():
@@ -13,17 +14,20 @@ def main():
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
     
-    #Saved cropped image of the players
-    for track_id, player in tracks['players'][0].items():
-        bbox = player['bbox']
-        frame = video_frames[0]
+    #Assign player team
+    team_assigner = TeamAssigner()
+    team_assigner.assing_team_color(video_frames[0],
+                                    tracks["players"][0])
+    
+    for frame_num, player_track in enumerate(tracks["players"]):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
-        #cropped bbox from frame
-        croppped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
 
-        #save it
-        cv2.imwrite(f'output_videos/cropped_image.jpg', croppped_image)
-        break
 
     
     #Draw output
