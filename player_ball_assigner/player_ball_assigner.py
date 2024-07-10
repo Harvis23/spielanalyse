72cdadb6ff88768495f551aa3155cc32f45b037e
Harvis Castellanos <harvis.castellanos@gmail.com>
2024-07-10 15:29:36 +0200

    Fix typo

--- player_ball_assigner/player_ball_assigner.py
+++ player_ball_assigner/player_ball_assigner.py
@@ -6,7 +6,7 @@ class PlayerBallAssigner():
     def __init__(self):
         self.max_player_ball_distance = 70
 
-    def assign_ball_to_plater(self, players, ball_bbox):
+    def assign_ball_to_player(self, players, ball_bbox):
         ball_position = get_center_of_bbox(ball_bbox)
 
         miniumum_distance = 99999
